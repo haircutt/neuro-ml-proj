75679e689f9176ff2059a7e6692128442b9ddc2a
Jessica Le <jjjyle1@gmail.com>
2026-07-03 01:56:20 +1000

    init: imported dataset + began viz

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-#!/usr/bin/env python3
+#!/usr/bin/env/python3
+
+import numpy as np
+from pathlib import Path
+
+import mne
+
+sample_data_folder = Path("pycrostates_2-20/data")
+
+print(sample_data_folder)
+sample_data_raw_file = sample_data_folder / "sub-010002_EC_avgref_1-30Hz_ep00_raw.fif"
+# sample_data_raw_file = sample_data_folder / "MEG" / "sample" / "sample_audvis_raw.fif"
+# raw = mne.io.read_raw_fif(sample_data_raw_file)
+# raw.crop(tmax=60).load_data()
+
+x = mne.io.read_raw(sample_data_folder / "sub-010002_EC_avgref_1-30Hz_ep00_raw.fif")
+data = x.get_data()
